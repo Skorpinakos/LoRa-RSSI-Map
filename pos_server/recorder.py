4e81fef5e54f10fa6a425827630a7a1e7de7f322
Ioannis Tsampras <gtsambras@gmail.com>
2023-07-21 22:13:02 +0300

    works 159 measurements

---
+++ pos_server/recorder.py
@@ -0,0 +1,53 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from datetime import datetime
+
+class JSONHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        
+        try:
+            json_data = json.loads(body)
+            self.save_json_to_file(json_data)
+            
+            #print(json_data)
+            # You can process the JSON data here or save it to a file, database, etc.
+            print("Received JSON data")
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = json.dumps({"status": "success"}).encode('utf-8')
+            self.wfile.write(response)
+        except Exception as e:
+            print('failed')
+            self.send_response(500)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = json.dumps({"status": "error", "message": str(e)}).encode('utf-8')
+            self.wfile.write(response)
+    def save_json_to_file(self, data):
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        filename = f"{timestamp}.json"
+
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+
+def run_server():
+    host = '0.0.0.0'
+    port = 8000
+    server_address = (host, port)
+
+    httpd = HTTPServer(server_address, JSONHandler)
+    print(f"Server started on http://{host}:{port}")
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    httpd.server_close()
+    print("Server stopped.")
+
+if __name__ == '__main__':
+    run_server()
---
+++ preprocessor.py
@@ -0,0 +1,116 @@
+import os
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+class Event:
+    def __init__(self,file_path):
+        #print(file_path)
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+            self.time=data["time"]
+            self.lat=data["object"]["location"]["latitude"]
+            self.lon=data["object"]["location"]["longitude"]
+            self.gateways=[]
+            self.rssis=[]
+            for gateway in data["rxInfo"]:
+                try:
+                    self.gateways.append([gateway["gatewayId"],gateway["rssi"],gateway["snr"]])
+                except:
+                    self.gateways.append([gateway["gatewayId"],gateway["rssi"]])
+                self.rssis.append(gateway["rssi"])
+                self.max_rssi=max(self.rssis)
+
+
+def get_json_files(directory):
+    json_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            json_files.append(directory+filename)
+    return json_files
+def filter_dragino(json_list):
+    filtered=[]
+    trash=[]
+    for file in json_list:
+        if is_word_present_in_json(file,"Dragino"):
+            filtered.append(file)
+        else:
+            trash.append(file)
+    print("packets:",len(filtered))
+    return filtered
+
+def is_word_present_in_json(file_path, word):
+    with open(file_path, 'r') as json_file:
+        data = str(json_file.read())
+    if word in data:
+        return True
+    else:
+        return False
+
+
+directory_path = "pos_server/" 
+json_files_list = get_json_files(directory_path)
+filtered_list=filter_dragino(json_files_list)
+points=[]
+for entry in filtered_list:
+    points.append(Event(entry))
+#print(points[0].max_rssi)
+points.sort(key=lambda x : x.time )
+with open('log.csv','a',encoding='utf-8') as file:
+    for point in points:
+        file.write(str(point.lat)+","+str(point.lon)+","+str(point.max_rssi/-1)+'\n')
+
+# Setup
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+params = {'font.size'     : 14,
+          'figure.figsize':(15.0, 8.0),
+          'lines.linewidth': 2.,
+          'lines.markersize': 15,}
+matplotlib.rcParams.update(params)
+
+Ni = len(points)
+Xi= np.array([x.lon for x in points])
+Xi=Xi-(sum(Xi)/len(Xi))
+Xi=Xi*100000
+Yi= np.array([x.lat for x in points])
+Yi=Yi-(sum(Yi)/len(Yi))
+Yi=Yi*100000
+Zi= np.array([x.max_rssi for x in points])
+print(Xi)
+
+
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+
+from scipy.spatial import Delaunay
+Pi = np.array([Xi, Yi]).transpose()
+tri = Delaunay(Pi)
+
+
+N = 4000
+x = np.linspace(-2000., 2000., N)
+y = np.linspace(-2000., 2000., N)
+X, Y = np.meshgrid(x, y)
+P = np.array([X.flatten(), Y.flatten() ]).transpose()
+#plt.plot(Xi, Yi, "or", label = "Data")
+#plt.triplot(Xi, Yi , tri.simplices.copy())
+#plt.plot(X.flatten(), Y.flatten(), "g,", label = "Z = ?")
+#plt.legend()
+#plt.grid()
+#plt.show()
+
+from scipy.interpolate import griddata
+Z_cubic = griddata(Pi, Zi, P, method = "cubic").reshape([N, N])
+plt.contourf(X, Y, Z_cubic, 100, cmap = mpl.cm.jet)
+plt.colorbar()
+#plt.contour(X, Y, Z_cubic, 20, colors = "k")
+#plt.triplot(Xi, Yi , tri.simplices.copy(), color = "k")
+plt.scatter(Xi, Yi, s=1)
+plt.legend()
+plt.grid()
+plt.show()
